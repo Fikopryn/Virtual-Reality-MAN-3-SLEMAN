867148529082a6ae18411e5b6c63ee41131e727e
Fiko <fikopriyono27@gmail.com>
2023-11-06 14:58:23 +0700

    Aset dan Bahan
    Asset dan bahan

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{ 
+     
+   
+    public void QuitApp()
+    {
+        Application.Quit();
+		Debug.Log("Quit!");
+    }
+}
